0015f773d813855f32088b789a26c29fdf4d4cfb
minidavarasl@gmail.com <minidavarasl@gmail.com>
2020-05-01 13:13:31 +0200

    added starting point and get gamer value

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-i don't know :D 
+const CHOICES = ['rock', 'paper', 'scissors']; 
+
+let computerChoice;
+
+// function to be called by the form on the html
+const submit = function () { 
+    /* randomly choose one of the 3 valid choices and assign it to the @computerChoice variable
+     * ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+     */
+    computerChoice = CHOICES[Math.floor(Math.random() * CHOICES.length)]; 
+}
+
+function getSelectValue ()
+{
+    let SelectedValue = document.getElementById("gamerchoice").value;
+}
