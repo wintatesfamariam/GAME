42259374a94f80ae7640b7f3c88444228864126b
WintaTesfamariam <winta.tesfamariam@live.se>
2020-05-01 17:15:00 +0200

    didnt modify anything really

--- javascript.js
+++ javascript.js
@@ -1,16 +1,15 @@
-const CHOICES = ['rock', 'paper', 'scissors']; 
+const CHOICES = ["rock", "paper", "scissors"];
 
 let computerChoice;
 
 // function to be called by the form on the html
-const submit = function () { 
-    /* randomly choose one of the 3 valid choices and assign it to the @computerChoice variable
-     * ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-     */
-    computerChoice = CHOICES[Math.floor(Math.random() * CHOICES.length)]; 
-}
+const submit = function () {
+  /* randomly choose one of the 3 valid choices and assign it to the @computerChoice variable
+   * ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+   */
+  computerChoice = CHOICES[Math.floor(Math.random() * CHOICES.length)];
+};
 
-function getSelectValue ()
-{
-    let SelectedValue = document.getElementById("gamerchoice").value;
+function getSelectValue() {
+  let SelectedValue = document.getElementById("gamerchoice").value;
 }
